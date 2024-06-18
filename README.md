09fd308bdcf07be29d14e0e8999db92e3b37521e
Nathan Hinthorne <nathanrh856@gmail.com>
2024-06-17 23:23:57 -0700

    setup MIDI input

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This is a simple MIDI visualizer that uses an LED keyboard to display the notes being played. It is written in Python and uses the `mido` library to read MIDI files and the `OpenRGB` library to light up keyboard LEDs. The visualizer is designed to work with the Redragon keyboard, but it can be easily modified for other keyboards.
 
+## Usage
+
+To use the visualizer, you must first edit the `config.json` file to match the LEDs on your keyboard. To identify which LEDs map to which keys, run `OpenRGB.exe` which you can install [here](https://openrgb.org/). 
+
+Next, ensure you have a MIDI device connected to your computer. Finally, start the visualizer by running the `main.py` file.
+
 ## Dependencies
 
 This application requires OpenRGB to be installed and running. You can download OpenRGB from [here](https://openrgb.org/).
--- main.py
+++ main.py
@@ -1,79 +1,153 @@
-import mido
 from openrgb import OpenRGB
+import mido
+import threading
 import subprocess
-
-# temp
 import time
-import random
+import json
+
 
 client = OpenRGB('localhost', 6742) # open connection on port 6742 (OpenRGB default port)
-
 devices = client.devices()
 
 # get only the keyboard device
-keyboard = next(devices)
-print('{} has {} LEDs'.format(keyboard.name, len(keyboard.leds)))
+keyboard = next(devices) # change depending on where the keyboard is in the list
+print('Connected to {}!\n'.format(keyboard.name))
+all_led_ids = [i for i in range(len(keyboard.leds))]
+
+def load_config():
+    with open('config.json') as f:
+        config = json.load(f)
+    return config
+
+# load config data
+config = load_config()
+midi_port_name = config['midi_port_name']
+colors = config['colors']
+note_mappings = config['note_mappings']
+
+# determine which leds are in the numpad and keyboard area
+numpad_led_ids = [note_data['led_id'] for note, note_data in note_mappings.items() if note_data['key'].startswith('num')]
+keyboard_led_ids = [note_data['led_id'] for note, note_data in note_mappings.items() if not (note_data['key'].startswith('num'))]
+
+# Mapping MIDI note numbers to note names
+NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+# create a map with led_id as the key
+led_colors = {}
+for note, note_data in note_mappings.items():
+    led_colors[note_data['led_id']] = colors['black']
 
 
-note_mappings = {
+def set_keyboard_color(new_color):
+    color_collection = []
+    for led_id in all_led_ids:
+        if led_id in keyboard_led_ids:
+            color_collection.append(new_color)
+            led_colors[led_id] = new_color
+        elif led_id in numpad_led_ids:
+            prev_color = led_colors[led_id]
+            color_collection.append(prev_color)
+        else:
+            color_collection.append((0, 0, 0))
 
-    # row 1
-    'C6': {'key': '0', 'led_id': 31, 'color': (0, 0, 0)},
-    'B5': {'key': '9', 'led_id': 30, 'color': (0, 0, 0)},
-    'A#5': {'key': '8', 'led_id': 29, 'color': (0, 0, 0)},
-    'A5': {'key': '7', 'led_id': 28, 'color': (0, 0, 0)},
-    'G#5': {'key': '6', 'led_id': 27, 'color': (0, 0, 0)},
-    'G5': {'key': '5', 'led_id': 26, 'color': (0, 0, 0)},
-    'F#5': {'key': '4', 'led_id': 25, 'color': (0, 0, 0)},
-    'F5': {'key': '3', 'led_id': 24, 'color': (0, 0, 0)},
-    'E5': {'key': '2', 'led_id': 23, 'color': (0, 0, 0)},
-    'D#5': {'key': '1', 'led_id': 22, 'color': (0, 0, 0)},
+    client.update_leds(color_collection)
 
-    # row 2
-    'D5': {'key': 'p', 'led_id': 52, 'color': (0, 0, 0)},
-    'C#5': {'key': 'o', 'led_id': 51, 'color': (0, 0, 0)},
-    'C5': {'key': 'i', 'led_id': 50, 'color': (0, 0, 0)},
-    'B4': {'key': 'u', 'led_id': 49, 'color': (0, 0, 0)},
-    'A#4': {'key': 'y', 'led_id': 48, 'color': (0, 0, 0)},
-    'A4': {'key': 't', 'led_id': 47, 'color': (0, 0, 0)},
-    'G#4': {'key': 'r', 'led_id': 46, 'color': (0, 0, 0)},
-    'G4': {'key': 'e', 'led_id': 45, 'color': (0, 0, 0)},
-    'F#4': {'key': 'w', 'led_id': 44, 'color': (0, 0, 0)},
-    'F4': {'key': 'q', 'led_id': 43, 'color': (0, 0, 0)},
 
-    # row 3
-    'D4': {'key': ';', 'led_id': 73, 'color': (0, 0, 0)},
-    'C#4': {'key': 'l', 'led_id': 72, 'color': (0, 0, 0)},
-    'C4': {'key': 'k', 'led_id': 71, 'color': (0, 0, 0)},
-    'B3': {'key': 'j', 'led_id': 70, 'color': (0, 0, 0)},
-    'A#3': {'key': 'h', 'led_id': 69, 'color': (0, 0, 0)},
-    'A3': {'key': 'g', 'led_id': 68, 'color': (0, 0, 0)},
-    'G#3': {'key': 'f', 'led_id': 67, 'color': (0, 0, 0)},
-    'G3': {'key': 'd', 'led_id': 66, 'color': (0, 0, 0)},
-    'F#3': {'key': 's', 'led_id': 65, 'color': (0, 0, 0)},
-    'F3': {'key': 'a', 'led_id': 64, 'color': (0, 0, 0)},
+def set_numpad_color(new_color):
+    color_collection = []
+    for led_id in all_led_ids:
+        if led_id in numpad_led_ids:
+            color_collection.append(new_color)
+            led_colors[led_id] = new_color
+        elif led_id in keyboard_led_ids:
+            prev_color = led_colors[led_id]
+            color_collection.append(prev_color)
+        else:
+            color_collection.append((0, 0, 0))
 
-    # row 4
-    'D3': {'key': '/', 'led_id': 95, 'color': (0, 0, 0)},
-    'C#3': {'key': '.', 'led_id': 94, 'color': (0, 0, 0)},
-    'C3': {'key': ',', 'led_id': 93, 'color': (0, 0, 0)},
-    'B2': {'key': 'm', 'led_id': 92, 'color': (0, 0, 0)},
-    'A#2': {'key': 'n', 'led_id': 91, 'color': (0, 0, 0)},
-    'A2': {'key': 'b', 'led_id': 90, 'color': (0, 0, 0)},
-    'G#2': {'key': 'v', 'led_id': 89, 'color': (0, 0, 0)},
-    'G2': {'key': 'c', 'led_id': 88, 'color': (0, 0, 0)},
-    'F#2': {'key': 'x', 'led_id': 87, 'color': (0, 0, 0)},
-    'F2': {'key': 'z', 'led_id': 86, 'color': (0, 0, 0)},
+    client.update_leds(color_collection)
 
-    # numpad
+
+def set_led_color(led_id, new_color):
+    client.update_single_led(led_id, new_color)
+    led_colors[led_id] = new_color
+
+
+
+
+# --- MIDI FUNCTIONS ---
+def note_number_to_name(note_number):
+    note_name = NOTE_NAMES[note_number % 12]
+    octave = (note_number // 12) - 1  # MIDI note 0 is C-1
+    return f"{note_name}{octave}"
+
+def list_midi_ports():
+    # List available MIDI input ports
+    print("Available MIDI input ports:")
+    for port in mido.get_input_names():
+        print(port)
+    print()
+
+def listen_to_midi(port_name):
+    # Open the specified MIDI input port and listen for messages
+    with mido.open_input(port_name) as port:
+        print(f"Listening on {port_name}")
+        for message in port:
+            print_message(message)
+            handle_midi_message(message)
+
+def print_message(message):
+    if message.type == 'note_on':
+        note_name = note_number_to_name(message.note)
+        print(f"Note On: {note_name} Velocity: {message.velocity}")
+    elif message.type == 'note_off':
+        note_name = note_number_to_name(message.note)
+        print(f"Note Off: {note_name}")
+
+def handle_midi_message(message):
+    if message.type == 'note_on':
+        note_name = note_number_to_name(message.note)
+        note_data = note_mappings.get(note_name)
+        if note_data is None:
+            return
+        
+        led_id = note_data['led_id']
+        if led_id in keyboard_led_ids:
+            set_led_color(led_id, colors['blue'])
+        elif led_id in numpad_led_ids:
+            set_led_color(led_id, colors['yellow'])
+
+    elif message.type == 'note_off':
+        note_name = note_number_to_name(message.note)
+        note_data = note_mappings.get(note_name)
+        if note_data is None:
+            return
+        
+        led_id = note_data['led_id']
+        set_led_color(led_id, colors['black'])
+
+
+
+
+
+# --- MAIN ---
+if __name__ == '__main__':
+    set_keyboard_color(colors['black'])
+    set_numpad_color(colors['black'])
+
+    list_midi_ports()
+
+    # listen_to_midi(midi_port_name)
     
-    # 'n7': 59,
-    # 'n8': 60,
-    # 'n9': 61,
-    # 'n4': 80,
-    # 'n5': 81,
-    # 'n6': 82,
-    # 'n1': 101,
-    # 'n2': 102,
-    # 'n3': 103
-}
+    # Create a new thread that will run the handle_midi_messages function
+    midi_thread = threading.Thread(target=listen_to_midi, args=(midi_port_name,))
+
+    # Start the new thread
+    midi_thread.start()
+
+    # Wait for the thread to finish
+    midi_thread.join()
+
+    # reset the keyboard color
+    set_keyboard_color(colors['black'])
+    set_numpad_color(colors['black'])
